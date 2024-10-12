83f9d0ace4bcc7a9c56df266bc22a3e460a8f8da
NamanGupta05 <134197125+NamanGupta05@users.noreply.github.com>
2024-10-12 12:52:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,39 +17,39 @@ buttonAnimation(event.key);
 function makesound(key){
     switch (key) {
         case "a":
-          var tom1 = new Audio("E:/Javascript/sounds/tom-1.mp3");
+          var tom1 = new Audio("E:/Drum-kit/sounds/tom-1.mp3");
           tom1.play();
           break;
     
         case "s":
           
-          var tom2 = new Audio("E:/Javascript/sounds/tom-2.mp3");
+          var tom2 = new Audio("E:/Drum-kit/sounds/tom-2.mp3");
           tom2.play();
          
           break;
     
         case "d":
-          var tom3 = new Audio("E:/Javascript/sounds/tom-3.mp3");
+          var tom3 = new Audio("E:/Drum-kit/sounds/tom-3.mp3");
           tom3.play();
           break;
     
         case "f":
-          var tom4 = new Audio("E:/Javascript/sounds/tom-4.mp3");
+          var tom4 = new Audio("E:/Drum-kit/sounds/tom-4.mp3");
           tom4.play();
           break;
     
         case "g":
-          var snare = new Audio("E:/Javascript/sounds/snare.mp3");
+          var snare = new Audio("E:/Drum-kit/sounds/snare.mp3");
           snare.play();
           break;
     
         case "h":
-          var crash = new Audio("E:/Javascript/sounds/crash.mp3");
+          var crash = new Audio("E:/Drum-kit/sounds/crash.mp3");
           crash.play();
           break;
     
         case "j":
-          var kick = new Audio("E:/Javascript/sounds/kick-bass.mp3");
+          var kick = new Audio("E:/Drum-kit/sounds/kick-bass.mp3");
           kick.play();
           break;
     
